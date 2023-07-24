330afcc78ac618715c706932b69e87dbf4739371
tomasquintana-gj <tomas.quintana@gojiraf.com>
2023-07-24 15:09:40 -0300

    restore order

--- backend/src/routes/seller.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sellerLogin, autenticar } from '../controllers/seller.controller';
-
-const router = express.Router();
-
-router.post('/login', sellerLogin);
-//router.post('/autenticar', autenticar)
-
-export default router;
