5fe03df328f8f3919fcd88bf214e7598afb0dd90
tomasquintana-gj <tomquintana20@gmail.com>
2023-07-24 12:00:14 -0300

    ref/ change some logic

--- backend/src/controllers/products.controller.ts
+++ backend/src/controllers/products.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express';
-import Product from '../models/product.model'
+import dotenv from 'dotenv';
+import jwt, {Secret} from 'jsonwebtoken';
+import Product from '../models/product.model';
 import { uploadFile } from '../services';
+
+dotenv.config();
+
+const secretToken: Secret = process.env.JWT_SECRET!;
 
 const createProducts = async (req: any, res: any) => {
 
   //TODO: add more validation
   
-  const {name, category, stock, price} = req.body
+  const {name, category, stock, price} = req.body;
 
   interface Product{
     name: string;
@@ -22,21 +28,30 @@ const createProducts = async (req: any, res: any) => {
     stock: stock,
     price: price,
     img: ''
+  };
+
+  const token: string | undefined = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No se proporcionó un token' });
   }
 
   try {
-    
-    const productData = new Product(objProduct)
-    const productSave = await productData.save()
-    return res.json({
-      productSave
-    })
+    jwt.verify(token, secretToken);
 
-  } catch (error) {
-    res.status(500).json({ 
+    const productData = new Product(objProduct);
+    const productSave = await productData.save();
+
+    return res.status(200).json({ message: 
+      productSave
+    });
+
+  } catch (err) {
+    return res.status(500).json({ 
       error: 'An internal server error occurred.'
     });
   }
+  
 };
 
 const allProducts = async ( req: Request, res: Response) => {
@@ -50,8 +65,8 @@ const allProducts = async ( req: Request, res: Response) => {
 
 const imageProduct = async ( req: Request, res: Response) => {
 
-  const {id, colection} = req.params
-  const product = await Product.findById(id)
+  const {id, colection} = req.params;
+  const product = await Product.findById(id);
 
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
     res.status(400).json({msg:  'No files were uploaded.' });
@@ -59,14 +74,14 @@ const imageProduct = async ( req: Request, res: Response) => {
   }
 
   try {
-    const pathFile: any = await uploadFile(req.files, ['webp'], 'Products') 
-    product!.img = pathFile
+    const pathFile: any = await uploadFile(req.files, ['webp'], 'Products'); 
+    product!.img = pathFile;
 
-    const productSave = await product!.save()
+    const productSave = await product!.save();
 
     res.json({
-      product 
-    })   
+      productSave 
+    });   
 
   } catch (error) {
     res.status(500).json({ 
@@ -74,7 +89,7 @@ const imageProduct = async ( req: Request, res: Response) => {
     });
   }
   
-}
+};
 
 export {
   createProducts,
