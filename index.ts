a7ce7314cfcee8a4384f87243c2c325339848608
tomasquintana-gj <tomquintana20@gmail.com>
2023-07-29 00:18:54 -0300

    add cors and pass port to .env

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import swaggerJsonDoc from 'swagger-jsdoc';
 import swaggerUI from 'swagger-ui-express';
 import fileUpload from 'express-fileupload';
+import cors from 'cors';
 import routesProduct from './src/routes/products.routes';
 import routesSeller from './src/routes/seller.routes';
 import { options } from './src/docs/swaggerOptions';
@@ -25,10 +26,24 @@ app.use(fileUpload({
   createParentPath: true
 }));
 
+const whitelist = ['http://localhost:5173'];
+const corsOptions = {
+  origin: function (origin:any, callback:any) {
+    //TODO: mejorar el tipado de origin y callback
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
 app.use('/api/products', routesProduct);
 app.use('/api/seller', routesSeller);
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(spect));
 
-app.listen(4000, () => {
-  console.log('Servidor corriendo en el puerto 4000');
+app.listen(`${process.env.PORT}`, () => {
+  console.log('Servidor corriendo en el puerto', process.env.PORT);
 });
